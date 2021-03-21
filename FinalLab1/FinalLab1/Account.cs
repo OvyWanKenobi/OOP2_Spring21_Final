e3555b0a410fd96113bf808db2a45bcf580b1cb2
OvyWanKenobi <imovy1997@gmai.com>
2021-03-21 14:33:40 +0600

    commit

---
+++ FinalLab1/FinalLab1/Account.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalLab1
+{
+        abstract class Account
+        {
+            public string AccID { get; set; }
+            public string AccName { get; set; }
+            public double Balance { get; set; }
+
+            public Account() { }
+            public Account(string accId, string accName, double accBalance)
+            {
+                AccID = accId;
+                AccName = accName;
+                Balance = accBalance;
+            }
+
+            public void Deposit(double amount)
+            {
+                Balance += amount;
+                Console.WriteLine("your account is deposited by {0}. Current balance: {1}", amount, Balance);
+            }
+
+            public  void Withdraw(double amount)
+             {
+            if (amount < Balance)
+            {
+                Balance -= amount;
+                Console.WriteLine("Your account debited by {0}", amount);
+            }
+            else
+            {
+                Console.WriteLine("Insufficient Balance");
+            }
+             }
+
+        public void Transfer(Account acc, double amount)
+        {
+            if (amount < Balance)
+            {
+                Balance -= amount;
+                acc.Balance += amount;
+                Console.WriteLine("your account transfer by amount {0} to the account {1}({2})", amount, acc.AccName, acc.AccNo);
+            }
+            else
+            {
+                Console.WriteLine("Insufficient Balance");
+            }
+        }
+
+        public void ShowInfo()
+            {
+                
+            }
+
+        }
+    }
---
+++ FinalLab1/FinalLab1/Transaction.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalLab1
+{
+    class Transaction
+    {
+        public Account Sender {
+            get { return sender; }
+            set { sender = value; }
+        }
+
+        public Account Receiver
+        {
+            get { return receiver; }
+            set { receiver = value; }
+        }
+  
+        public int Amount
+        {
+            get { return amount; }
+            set { amount = value; }
+        }
+
+        public string AdditionalInfo
+        {
+            get { return additionalInfo; }
+            set { additionalInfo = value; }
+        }
+
+
+        public Transaction() { }
+        public Transaction(Account sender, Account receiver, int amount, string additionalInfo)
+        {
+           
+        }
+
+    }
+}
